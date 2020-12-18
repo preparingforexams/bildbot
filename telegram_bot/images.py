5eaa225affd64231418bcf95f4e6c45f5e17736c
Torben Carstens <github@carstens.tech>
2020-12-18 19:36:45 +0100

    Don't return user images

--- telegram_bot/images.py
+++ telegram_bot/images.py
@@ -1,5 +1,5 @@
 import random
-from typing import Set, List
+from typing import List
 
 import requests
 
@@ -15,14 +15,17 @@ def get_dirty() -> List[str]:
     import re
 
     links = set()
-    response = requests.get("https://photos.google.com/share/AF1QipNS94mSZRF81m0SaNwqEe5FvV3aQ0Zm9zbOp-HNg6s5EU4upTuqnrZj2-_VSyLn8Q?key=Qk9VUDlXVHZqRGtWOFo1aUxVXzdReElsU2NuNExR")
+    response = requests.get(
+        "https://photos.google.com/share/AF1QipNS94mSZRF81m0SaNwqEe5FvV3aQ0Zm9zbOp-HNg6s5EU4upTuqnrZj2-_VSyLn8Q?key=Qk9VUDlXVHZqRGtWOFo1aUxVXzdReElsU2NuNExR")
     content = response.content.decode("utf-8")
     for find in re.findall('<img([^>]*)', content):
         link = re.search('(https://lh3.googleusercontent.com/[^"]+)', find).group()
         link = re.sub(r"=w\d+-h\d+(:?-no)?", "", link)
         link = re.sub(r"=s\d+(:?-no)?", "", link)
-        if 'https://lh3.googleusercontent.com/a/' in link or ('width="32"' in find[0] and 'height="32"' in find[0]) or link == "https://lh3.googleusercontent.com/YhfpzamTTNKpbiVWUZjP8T0URrfTdxD-iQS0iy1Q00vPYgpYP-TvTGpsXlpjNXGcZd9kC_SC_pF4kWbHRwpHXpqXLH5QjEocn6-HpkSiDqOf":
+        if 'https://lh3.googleusercontent.com/a/' in link or (
+                'width="32"' in find[0] and 'height="32"' in find[0]) or "title" in find:
             continue
+
         links.add(link)
 
     return list(links)
